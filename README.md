c742b4dc405a09068117032288e2a068410317bb
shoom3301 <shoom3301@gmail.com>
2021-04-21 18:26:19 +0600

    Init LimitOrderContract sdk

--- README.md
+++ README.md
@@ -1,1 +1,156 @@
 # SDK for limit orders protocol
+
+## How to start
+
+Create `LimitOrderContract` instance:
+```typescript
+const contractAddress = '0xabc...';
+const chainId = 1;
+const web3 = new Web3();
+
+const limitorderContract = new LimitOrderContract(
+    contractAddress,
+    chainId,
+    new Web3ProviderConnector(web3)
+);
+```
+
+
+**Note:** you can use any implementation for the provider.  
+Just implement `ProviderConnector` interface:
+```typescript
+class MyProviderConnector implements ProviderConnector {
+    //...
+}
+```
+
+## Create a limit order
+```typescript
+class LimitOrderManager {
+    limitorderContract: LimitOrderContract;
+    walletAddress: string;
+
+    async createOrder(
+        makerAssetAddress: string,
+        takerAssetAddress: string,
+        makerAmount: number,
+        takerAmount: number,
+        expireTimeSeconds: number
+    ): Promise<void> {
+        const predicate = await this.buildNewOrderPredicate(expireTimeSeconds);
+
+        const order = this.limitorderContract.buildOrder({
+            makerAddress: this.walletAddress,
+            makerAssetAddress,
+            takerAssetAddress,
+            makerAmount: this.tokenAmountToUnits(makerAssetAddress, makerAmount).toString(),
+            takerAmount: this.tokenAmountToUnits(takerAssetAddress, takerAmount).toString(),
+            predicate
+        });
+
+        const typedData = this.limitorderContract.buildOrderTypedData(order);
+
+        const hash = this.limitorderContract.getOrderHash(typedData);
+
+        const signature = await this.limitorderContract.getOrderSignature(this.walletAddress, typedData);
+
+        console.log('New order: ', {order, hash, signature});
+    }
+
+    async buildNewOrderPredicate(expireTimeSeconds: number): Promise<string> {
+        const timestampBelow = Math.floor(Date.now() / 1000) + expireTimeSeconds;
+
+        const nonce = await this.limitorderContract.nonces(this.walletAddress);
+
+        const noncePredicate = this.limitorderContract.nonceEquals(this.walletAddress, nonce);
+
+        const timestampPredicate = this.limitorderContract.timestampBelow(timestampBelow);
+
+        return this.limitorderContract.andPredicate([
+            noncePredicate,
+            timestampPredicate
+        ]);
+    }
+}
+```
+
+## Fill a limit order:
+```typescript
+class LimitOrderManager {
+    getEntity(orderHash: LimitOrderHash): {order: LimitOrder, signature: LimitOrderSignature} {
+        // Get limit order by hash
+    }
+
+    tokenAmountToUnits(tokenAddress: string, amount: number): string {
+        // Format token amount to units
+    }
+
+    sendTransaction(callData: string): void {
+        // Send transaction to blockchain
+    }
+
+    fillOrder(orderHash: LimitOrderHash, makerAmount: number, takerAmount: number): void {
+        const {order, signature} = this.getEntity(orderHash);
+
+        const callData = this.limitorderContract.fillOrder(
+            order,
+            signature,
+            this.tokenAmountToUnits(order.makerAsset, makerAmount),
+            this.tokenAmountToUnits(order.takerAsset, takerAmount)
+        );
+
+        this.sendTransaction(callData);
+    }
+}
+```
+
+## Cancel a limit order:
+```typescript
+class LimitOrderManager {
+    getEntity(orderHash: LimitOrderHash): {order: LimitOrder, signature: LimitOrderSignature} {
+        // Get limit order by hash
+    }
+
+    sendTransaction(callData: string): void {
+        // Send transaction to blockchain
+    }
+
+    cancelOrder(orderHash: LimitOrderHash): void {
+        if (this.limitorderContract === null) {
+            return;
+        }
+
+        const {order} = this.getEntity(orderHash);
+
+        const callData = this.limitorderContract.cancelOrder(order);
+
+        this.sendTransaction(callData);
+    }
+}
+```
+
+## Cancel all orders:
+```typescript
+class LimitOrderManager {
+    sendTransaction(callData: string): void {
+        // Send transaction to blockchain
+    }
+
+    cancelAllOrders(): void {
+        const callData = this.limitorderContract.advanceNonce();
+
+        this.sendTransaction(callData);
+    }
+}
+```
+
+## Get the remainder of a limit order:
+```typescript
+class LimitOrderManager {
+    async remaining(orderHash: LimitOrderHash): Promise<void> {
+        const remaining = await this.limitorderContract.remaining(orderHash);
+
+        console.log('Order remaining', remaining);
+    }
+}
+```
---
+++ src/limit-order-contract.ts
@@ -0,0 +1,207 @@
+import {
+    EIP712_DOMAIN,
+    ORDER_STRUCTURE,
+    PROTOCOL_VERSION,
+    ZERO_ADDRESS,
+    PROTOCOL_NAME,
+    LIMIT_ORDER_PROTOCOL_ABI,
+    ERC20_ABI, ZX
+} from './limit-order-contract.const';
+import {
+    ChainId,
+    LimitOrder,
+    LimitOrderContractMethods, LimitOrderData,
+    LimitOrderHash,
+    LimitOrderSignature
+} from './model/limit-order-contract.model';
+import {EIP712TypedData} from './model/eip712.model';
+import {TypedDataUtils} from 'eth-sig-util';
+import {ProviderConnector} from './provider-connector';
+import {BigNumber} from '@ethersproject/bignumber';
+
+export class LimitOrderContract {
+    constructor(
+        private readonly contractAddress: string,
+        private readonly chainId: ChainId,
+        private readonly providerConnector: ProviderConnector,
+    ) {
+    }
+
+    getOrderSignature(walletAddress: string, typedData: EIP712TypedData): Promise<LimitOrderSignature> {
+        return this.providerConnector.signTypedData(walletAddress, typedData);
+    }
+
+    getOrderHash(orderTypedData: EIP712TypedData): LimitOrderHash {
+        return ZX + TypedDataUtils.sign(orderTypedData as any).toString('hex');
+    }
+
+    buildOrderTypedData(order: LimitOrder): EIP712TypedData {
+        return {
+            primaryType: 'Order',
+            types: {
+                EIP712Domain: EIP712_DOMAIN,
+                Order: ORDER_STRUCTURE
+            },
+            domain: {
+                name: PROTOCOL_NAME,
+                version: PROTOCOL_VERSION,
+                chainId: this.chainId,
+                verifyingContract: this.contractAddress
+            },
+            message: order,
+        };
+    }
+
+    buildOrder(data: LimitOrderData): LimitOrder {
+        const {
+            makerAssetAddress,
+            takerAssetAddress,
+            makerAddress,
+            takerAddress = ZERO_ADDRESS,
+            makerAmount,
+            takerAmount,
+            predicate = ZX,
+            permit = ZX,
+            interaction = ZX
+        } = data;
+
+        return {
+            salt: this.generateSalt(),
+            makerAsset: makerAssetAddress,
+            takerAsset: takerAssetAddress,
+            makerAssetData: this.transferFrom(makerAssetAddress, makerAddress, takerAddress, makerAmount),
+            takerAssetData: this.transferFrom(makerAssetAddress, takerAddress, makerAddress, takerAmount),
+            getMakerAmount: this.getAmountData(LimitOrderContractMethods.getMakerAmount, makerAmount, takerAmount),
+            getTakerAmount: this.getAmountData(LimitOrderContractMethods.getTakerAmount, makerAmount, takerAmount),
+            predicate,
+            permit,
+            interaction
+        };
+    }
+
+    fillOrder(
+        order: LimitOrder,
+        signature: LimitOrderSignature,
+        makerAmount: string,
+        takerAmount: string
+    ): string {
+        return this.getContractCallData(LimitOrderContractMethods.fillOrder, [
+            order,
+            signature,
+            makerAmount,
+            takerAmount
+        ]);
+    }
+
+    cancelOrder(order: LimitOrder): string {
+        return this.getContractCallData(LimitOrderContractMethods.cancelOrder, [
+            order
+        ]);
+    }
+
+    nonces(makerAddress: string): Promise<number> {
+        const callData = this.getContractCallData(LimitOrderContractMethods.nonces, [
+            makerAddress
+        ]);
+
+        return this.providerConnector.ethCall(this.contractAddress, callData)
+            .then(nonce => BigNumber.from(nonce).toNumber());
+    }
+
+    advanceNonce(): string {
+        return this.getContractCallData(LimitOrderContractMethods.advanceNonce);
+    }
+
+    andPredicate(predicates: string[]): string {
+        return this.getContractCallData(LimitOrderContractMethods.and, [
+            predicates.map(() => this.contractAddress),
+            predicates
+        ]);
+    }
+
+    timestampBelow(timestamp: number): string {
+        return this.getContractCallData(LimitOrderContractMethods.timestampBelow, [
+            timestamp
+        ]);
+    }
+
+    nonceEquals(makerAddress: string, makerNonce: number): string {
+        return this.getContractCallData(LimitOrderContractMethods.nonceEquals, [
+            makerAddress,
+            makerNonce
+        ]);
+    }
+
+    remaining(hash: LimitOrderHash): Promise<BigNumber | string> {
+        const callData = this.getContractCallData(LimitOrderContractMethods.remaining, [
+            hash
+        ]);
+
+        return this.providerConnector.ethCall(this.contractAddress, callData)
+            .then(result => {
+            if (result.length === 66) {
+                return BigNumber.from(result);
+            }
+
+            // Parse error
+            const parsed = this.providerConnector.decodeABIParameter('string', ZX + result.slice(10));
+
+            return Promise.reject(parsed);
+        });
+    }
+
+    remainingsRaw(hashes: LimitOrderHash[]): Promise<string[]> {
+        return this.providerConnector.contractCall<string[]>(
+            LIMIT_ORDER_PROTOCOL_ABI,
+            this.contractAddress,
+            LimitOrderContractMethods.remainingsRaw,
+            [hashes],
+            'latest'
+        );
+    }
+
+    private generateSalt(): string {
+        return Math.round(Math.random() * Date.now()) + '';
+    }
+
+    private transferFrom(
+        makerAssetAddress: string,
+        fromAddress: string,
+        toAddress: string,
+        value: string
+    ): string {
+        return this.providerConnector.contractEncodeABI(
+            ERC20_ABI,
+            makerAssetAddress,
+            'transferFrom',
+            [
+                fromAddress,
+                toAddress,
+                value,
+            ]
+        );
+    }
+
+    // Get nonce from contract (nonces method) and put it to predicate on order creating
+    private getAmountData(
+        methodName: LimitOrderContractMethods,
+        makerAmount: string,
+        takerAmount: string,
+        swapTakerAmount = '0'
+    ): string {
+        return this.getContractCallData(methodName, [
+            makerAmount,
+            takerAmount,
+            swapTakerAmount
+        ]).substr(0, 2 + 68 * 2);
+    }
+
+    private getContractCallData(methodName: LimitOrderContractMethods, methodParams: any[] = []): string {
+        return this.providerConnector.contractEncodeABI(
+            LIMIT_ORDER_PROTOCOL_ABI,
+            this.contractAddress,
+            methodName,
+            methodParams
+        );
+    }
+}
