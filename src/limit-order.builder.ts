4e54e522a07b3ffd1dbbaef09254c197db9a9ce0
shoom3301 <shoom3301@gmail.com>
2021-04-22 17:15:11 +0600

    Separate LimitOrderProtocolFacade and LimitOrderBuilder

--- src/limit-order.builder.ts
+++ src/limit-order.builder.ts
@@ -18,9 +18,8 @@ import {
 import {EIP712TypedData} from './model/eip712.model';
 import {TypedDataUtils} from 'eth-sig-util';
 import {ProviderConnector} from './provider-connector';
-import {BigNumber} from '@ethersproject/bignumber';
 
-export class LimitOrderProtocolUtils {
+export class LimitOrderBuilder {
     constructor(
         private readonly contractAddress: string,
         private readonly chainId: ChainId,
@@ -28,11 +27,11 @@ export class LimitOrderProtocolUtils {
     ) {
     }
 
-    getOrderSignature(walletAddress: string, typedData: EIP712TypedData): Promise<LimitOrderSignature> {
+    buildOrderSignature(walletAddress: string, typedData: EIP712TypedData): Promise<LimitOrderSignature> {
         return this.providerConnector.signTypedData(walletAddress, typedData);
     }
 
-    getOrderHash(orderTypedData: EIP712TypedData): LimitOrderHash {
+    buildOrderHash(orderTypedData: EIP712TypedData): LimitOrderHash {
         return ZX + TypedDataUtils.sign(orderTypedData as any).toString('hex');
     }
 
@@ -78,87 +77,6 @@ export class LimitOrderProtocolUtils {
             permit,
             interaction
         };
-    }
-
-    fillOrder(
-        order: LimitOrder,
-        signature: LimitOrderSignature,
-        makerAmount: string,
-        takerAmount: string
-    ): string {
-        return this.getContractCallData(LimitOrderProtocolMethods.fillOrder, [
-            order,
-            signature,
-            makerAmount,
-            takerAmount
-        ]);
-    }
-
-    cancelOrder(order: LimitOrder): string {
-        return this.getContractCallData(LimitOrderProtocolMethods.cancelOrder, [
-            order
-        ]);
-    }
-
-    nonces(makerAddress: string): Promise<number> {
-        const callData = this.getContractCallData(LimitOrderProtocolMethods.nonces, [
-            makerAddress
-        ]);
-
-        return this.providerConnector.ethCall(this.contractAddress, callData)
-            .then(nonce => BigNumber.from(nonce).toNumber());
-    }
-
-    advanceNonce(): string {
-        return this.getContractCallData(LimitOrderProtocolMethods.advanceNonce);
-    }
-
-    andPredicate(predicates: string[]): string {
-        return this.getContractCallData(LimitOrderProtocolMethods.and, [
-            predicates.map(() => this.contractAddress),
-            predicates
-        ]);
-    }
-
-    timestampBelow(timestamp: number): string {
-        return this.getContractCallData(LimitOrderProtocolMethods.timestampBelow, [
-            timestamp
-        ]);
-    }
-
-    nonceEquals(makerAddress: string, makerNonce: number): string {
-        return this.getContractCallData(LimitOrderProtocolMethods.nonceEquals, [
-            makerAddress,
-            makerNonce
-        ]);
-    }
-
-    remaining(hash: LimitOrderHash): Promise<BigNumber | string> {
-        const callData = this.getContractCallData(LimitOrderProtocolMethods.remaining, [
-            hash
-        ]);
-
-        return this.providerConnector.ethCall(this.contractAddress, callData)
-            .then(result => {
-            if (result.length === 66) {
-                return BigNumber.from(result);
-            }
-
-            // Parse error
-            const parsed = this.providerConnector.decodeABIParameter('string', ZX + result.slice(10));
-
-            return Promise.reject(parsed);
-        });
-    }
-
-    remainingsRaw(hashes: LimitOrderHash[]): Promise<string[]> {
-        return this.providerConnector.contractCall<string[]>(
-            LIMIT_ORDER_PROTOCOL_ABI,
-            this.contractAddress,
-            LimitOrderProtocolMethods.remainingsRaw,
-            [hashes],
-            'latest'
-        );
     }
 
     private generateSalt(): string {
