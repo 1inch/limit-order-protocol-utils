ae18920b6010894eb3605ae7cc9e814d63241f37
limitofzero <limitofzero@gmail.com>
2023-09-17 21:02:18 +0300

    fix(export): export v3 classes

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ export * from './erc20.facade';
 export * from './limit-order-protocol.const';
 export * from './limit-order.builder';
 export * from './limit-order-protocol.facade';
+export * from './limit-order-protocol-v3.facade';
 export * from './limit-order-predicate.builder';
+export * from './limit-order-predicate-v3.builder';
 
 export * from './limit-order.decoder';
 export * from './limit-order-predicate.decoder';
