ceea00615f7bc43b62f3b4dae30d83a972541c92
limitofzero <limitofzero@gmail.com>
2024-01-16 20:23:15 +0400

    chore(): remove unused

--- src/e2e-tests/tests/helpers/fixtures.ts
+++ src/e2e-tests/tests/helpers/fixtures.ts
@@ -8,16 +8,11 @@ export async function deploySwapTokens () {
     const weth = await WrappedTokenMock.deploy('WETH', 'WETH');
 
     await weth.waitForDeployment();
-    const inch = await TokenMock.deploy('1INCH', '1INCH');
-    await inch.waitForDeployment();
     const LimitOrderProtocol = await ethers.getContractFactory('LimitOrderProtocol');
     const swap = await LimitOrderProtocol.deploy(await weth.getAddress());
     await swap.waitForDeployment();
-    const TokenCustomDecimalsMock = await ethers.getContractFactory('TokenCustomDecimalsMock');
-    const usdc = await TokenCustomDecimalsMock.deploy('USDC', 'USDC', '0', 6);
-    await usdc.waitForDeployment();
     const chainId = (await ethers.provider.getNetwork()).chainId;
-    return { dai, weth, inch, swap, chainId, usdc };
+    return { dai, weth, swap, chainId };
 };
 
 export async function deployArbitraryPredicate () {
